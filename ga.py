e457776aad23d7bd9e1140c7b73ff845ac5511af
RexSocrates <socrateshung053@gmail.com>
2018-06-22 21:16:25 +0800

    Reach Mutation

--- ga.py
+++ ga.py
@@ -22,13 +22,25 @@ def evaluate(individual):
 
 # choose the crossover function as uniform
 toolbox.register("mate", tools.cxUniform) 
+# mutation function
 toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1) 
-toolbox.register("select", tools.selTournament, tournsize=3) 
+# selection of next generation
+toolbox.register("select", tools.selBest, tournsize=3) 
+# evaluate the fitness value of the chromosome
 toolbox.register("evaluate", evaluate)
 
 def main():
     pop = toolbox.population(n=50)
     # print(pop)
+
+    # crossover rate
+    CXPB = 0.5
+
+    # mutation rate
+    MUTPB = 0.2
+
+    # number of generations
+    NGEN = 1000
 
     fitness = list(map(toolbox.evaluate, pop))
     for ind, fit in zip(pop, fitness) :
@@ -36,6 +48,28 @@ def main():
     	print(fit)
 
     fits = [ind.fitness.value[0] for ind in pop]
+
+    generations = 0
+
+    while generations < NGEN :
+    	generations = generations + 1
+    	print("Generation %i" % generations)
+
+    	# select the next generation
+    	offspring = toolbox.select(pop, len(pop))
+    	# Clone the selected individuals
+    	offspring = list(map(toolbox.clone, offspring))
+
+    	for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
 
 
 
